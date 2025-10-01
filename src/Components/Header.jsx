1323a45cfc141e93a0d41311953463a5e69af343
Reiad <reiadwc@gmail.com>
2025-10-01 00:42:37 -0400

    Initial Commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,21 @@
+
+function Header(){
+    return (
+        <header> 
+            <div className="logo">
+                <h1> AfterShock </h1>
+            </div>
+            <nav> 
+                <li className="nav-container"> 
+                    <a className="nav-link"> Home </a>
+                    <a className="nav-link"> About </a>
+                    <a className="nav-link"> Services</a>
+                    <a className="nav-link"> Contact </a>
+                </li>
+            </nav>
+        </header>
+        
+    )
+
+}
+export default Header;
