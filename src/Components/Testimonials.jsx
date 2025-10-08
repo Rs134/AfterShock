9457d414948d938ad21be7c4316660ee0cee96e2
Reiad <reiadwc@gmail.com>
2025-10-07 23:47:16 -0400

    Utilized flex layout on reviews

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -2,6 +2,23 @@ function Testimonials (){
     return (
         <div className="test-container">
             <h1 className="title test-title"> Testimonials</h1>
+            <div className="reviews">
+                <div className="review-box">
+                    <h1 className="name"> Test</h1>
+                    <p className="description"> Test</p>
+                    <img src="" />
+                </div>
+                <div className="review-box">
+                    <h1 className="name"> Test</h1>
+                    <p className="description"> Test</p>
+                    <img src="" />
+                </div>
+                <div className="review-box">
+                    <h1 className="name"> Test</h1>
+                    <p className="description"> Test</p>
+                    <img src="" />
+                </div>
+            </div>
         </div>
     )
 }
