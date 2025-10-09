e8276f982bc0e232b908891dd0b1365685f4ca8b
Reiad <reiadwc@gmail.com>
2025-10-08 21:48:37 -0400

    Updated styling for testimonials

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -4,18 +4,18 @@ function Testimonials (){
             <h1 className="title test-title"> Testimonials</h1>
             <div className="reviews">
                 <div className="review-box">
-                    <h1 className="name"> Testvververv</h1>
-                    <p className="description"> Test</p>
+                    <h1 className="name"> Review #1 </h1>
+                    <p className="description"> Aftershock helped me process the emotional impact of my accident in a way that felt safe and supportive. It gave me clear resources and steps I could take to move forward.</p>
                     <img src="" />
                 </div>
                 <div className="review-box">
-                    <h1 className="name"> Testvververv</h1>
-                    <p className="description"> Test</p>
+                    <h1 className="name"> Review #1</h1>
+                    <p className="description"> This project combines compassion with practical guidance in a way I haven’t seen before. It felt like someone truly understood what I was going through.</p>
                     <img src="" />
                 </div>
                 <div className="review-box">
-                    <h1 className="name"> Testvververv</h1>
-                    <p className="description"> Test</p>
+                    <h1 className="name"> Review #1</h1>
+                    <p className="description"> Aftershock made me feel less alone during a really difficult time. The personalized insights and recommendations were exactly what I needed.</p>
                     <img src="" />
                 </div>
             </div>
