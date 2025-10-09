8447f7e3870958e3c7740bc65eb925ee805aa2c2
Reiad <reiadwc@gmail.com>
2025-10-08 22:14:38 -0400

    Finished testimonial section

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -5,18 +5,18 @@ function Testimonials (){
             <div className="reviews">
                 <div className="review-box">
                     <h1 className="name"> Review #1 </h1>
-                    <p className="description"> Aftershock helped me process the emotional impact of my accident in a way that felt safe and supportive. It gave me clear resources and steps I could take to move forward.</p>
-                    <img src="" />
+                    <p className="description"> Aftershock helped me with the emotional impact of my accident in a way that felt safe and supportive. It provided me with a way to move forward.</p>
+                    <img className="review-img" src="avatar.jpg" />
                 </div>
                 <div className="review-box">
-                    <h1 className="name"> Review #1</h1>
+                    <h1 className="name"> Review #2</h1>
                     <p className="description"> This project combines compassion with practical guidance in a way I haven’t seen before. It felt like someone truly understood what I was going through.</p>
-                    <img src="" />
+                    <img className="review-img" src="avatar.jpg" />
                 </div>
                 <div className="review-box">
-                    <h1 className="name"> Review #1</h1>
+                    <h1 className="name"> Review #3</h1>
                     <p className="description"> Aftershock made me feel less alone during a really difficult time. The personalized insights and recommendations were exactly what I needed.</p>
-                    <img src="" />
+                    <img className="review-img" src="avatar.jpg" />
                 </div>
             </div>
         </div>
